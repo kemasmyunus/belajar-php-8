34e15b1bdd9f2e3305f0d8694dee1c5c4e2727e6
kemasmyunus <7a9tqplxjh@gmail.com>
2025-02-18 05:13:42 +0800

    update: belajar attributes

--- Attribute.php
+++ Attribute.php
@@ -12,3 +12,28 @@ class LoginRequest{
     #[NotBlank]
     public string $password;
 }
+
+function validate(object $object){
+    $class = new ReflectionClass($object);
+    $properties = $class->getProperties();
+    foreach ($properties as $property){
+        validateNotBlank($property, $object);
+    }
+}
+
+function validateNotBlank(ReflectionProperty $property, object $object): void{
+    $attributes = $property->getAttributes(NotBlank::class);
+    if(count($attributes)>0){
+        if(!$property->isInitialized($object)){
+            throw new Exception("Property $property->name is null");
+        }
+        if($property->getValue($object) == null){
+            throw new Exception("Property $property->name is null");
+        }
+    }
+}
+
+$request = new LoginRequest();
+$request->username = "kousaka";
+$request->password = "kitabisa";
+validate($request);
